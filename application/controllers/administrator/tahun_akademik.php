dd0fb91a414a0e6d73242404d8f30e5f42b43fe2
Anas Khalif <anaskhalif995@gmail.com>
2023-05-23 12:33:19 +0700

    fix : CRUD halaman Tahun Akademik

--- application/controllers/administrator/tahun_akademik.php
+++ application/controllers/administrator/tahun_akademik.php
@@ -59,4 +59,54 @@ class Tahun_akademik extends CI_Controller{
 			'required'	=> 'Status Wajib Diisi!'
 		]);
 	}
+
+	public function update($id)
+	{
+		$where = array('id' => $id);
+		$data['tahun_akademik'] = $this->tahunakademik_model->edit_data($where, 'tahun_akademik')->result();
+		$this->load->view('templates_administrator/header');
+		$this->load->view('templates_administrator/sidebar');
+		$this->load->view('administrator/tahun_akademik_update',$data);
+		$this->load->view('templates_administrator/footer');
+	}
+
+	public function update_aksi()
+	{
+		$id = $this->input->post('id');
+		$tahun_akademik = $this->input->post('tahun_akademik');
+		$semester = $this->input->post('semester');
+		$status = $this->input->post('status');
+
+		$data = array(
+			'tahun_akademik' => $tahun_akademik,
+			'semester'		 => $semester,
+			'status' 		 => $status
+		);
+
+		$where = array(
+			'id' => $id
+		);
+
+		$this->tahunakademik_model->update_data($where,$data,'tahun_akademik');
+		$this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                Data Tahun Akademik Berhasil Diupdate!
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+			redirect('administrator/tahun_akademik');
+	}
+
+	public function delete($id)
+	{
+		$where = array('id' => $id);
+		$this->tahunakademik_model->hapus_data($where,'tahun_akademik');
+		$this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                Data Tahun Akademik Berhasil Hapus!
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+			redirect('administrator/tahun_akademik');
+	}
 }
